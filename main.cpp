c3d4f36888c54cc37908a7a5bcaaa207c8f348b0
coon <coon@c-base.org>
2018-06-30 21:03:27 +0200

    map mms notes to midi notes

--- main.cpp
+++ main.cpp
@@ -22,29 +22,31 @@ static int mssNote2midiNote(const char* pMssNote) {
     pMssNote++;
   }
 
-  // TODO: return correct MIDI notes:
-  
-  switch(pMssNote[0]) {
-    case 'A': return 0; // A4 (440 Hz)
-    case 'B': return 0; // B4
-    case 'C': return 0; // C5
-    case 'D': return 0; // D5
-    case 'E': return 0; // E5
-    case 'F': return 0; // F5
-    case 'G': return 0; // G5
-    case 'H': return 0; // A5
-    case 'a': return 0; // B5
-    case 'b': return 0; // C6
-    case 'c': return 0; // D6
-    case 'd': return 0; // E6
-    case 'e': return 0; // F6
-    case 'f': return 0; // G6
-    case 'g': return 0; // A6
-    case 'h': return 0; // B6
-    case 'i': return 0; // C7
-  }
+  auto char2Note = [](char charNote) -> int {
+    switch(charNote) {
+      case 'A': return 69; // A4 (440 Hz)
+      case 'B': return 71; // B4
+      case 'C': return 72; // C5
+      case 'D': return 74; // D5
+      case 'E': return 76; // E5
+      case 'F': return 77; // F5
+      case 'G': return 79; // G5
+      case 'H': return 81; // A5
+      case 'a': return 83; // B5
+      case 'b': return 84; // C6
+      case 'c': return 86; // D6
+      case 'd': return 88; // E6
+      case 'e': return 89; // F6
+      case 'f': return 91; // G6
+      case 'g': return 93; // A6
+      case 'h': return 95; // B6
+      case 'i': return 96; // C7
+    }
 
-  return 0;
+    return 0;
+  };
+
+  return char2Note(pMssNote[0]) + semiTone;
 }
 
 int main() {
