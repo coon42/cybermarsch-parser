dc4a75e865249bf4fc9d42a8b506efc5b00fb6ae
coon <coon@c-base.org>
2018-06-30 20:39:21 +0200

    continue midi parsing...

--- main.cpp
+++ main.cpp
@@ -10,6 +10,43 @@ extern "C" {
 using namespace rapidxml;
 using namespace std;
 
+static int mssNote2midiNote(const char* pMssNote) {
+  int semiTone = 0;
+
+  if (pMssNote[0] == '-') {
+    semiTone = 1;
+    pMssNote++;
+  }
+  else if (pMssNote[0] == '+') {
+    semiTone = -1;
+    pMssNote++;
+  }
+
+  // TODO: return correct MIDI notes:
+  
+  switch(pMssNote[0]) {
+    case 'A': return 0; // A4 (440 Hz)
+    case 'B': return 0; // B4
+    case 'C': return 0; // C5
+    case 'D': return 0; // D5
+    case 'E': return 0; // E5
+    case 'F': return 0; // F5
+    case 'G': return 0; // G5
+    case 'H': return 0; // A5
+    case 'a': return 0; // B5
+    case 'b': return 0; // C6
+    case 'c': return 0; // D6
+    case 'd': return 0; // E6
+    case 'e': return 0; // F6
+    case 'f': return 0; // G6
+    case 'g': return 0; // A6
+    case 'h': return 0; // B6
+    case 'i': return 0; // C7
+  }
+
+  return 0;
+}
+
 int main() {
   const char* pFileName = "cybermarsch.xml";
 
@@ -34,12 +71,18 @@ int main() {
   xml_node<>* pRootNode = doc.first_node();
   cout << "Name of first node is: " << pRootNode->name() << "\n";
 
-  for(xml_node<>* pNode = pRootNode->first_node("chord"); pNode; pNode = pNode->next_sibling()) {
-    print(std::cout, *pNode, 0);
+  for (xml_attribute<>* pAttr = pRootNode->first_attribute(); pAttr; pAttr = pAttr->next_attribute()) {
+    cout << "Root node has attribute '" << pAttr->name() << "' ";
+    cout << "with value '" << pAttr->value() << "'\n";
+  }
 
-    for (xml_attribute<>* pAttr = pNode->first_attribute(); pAttr; pAttr = pAttr->next_attribute()) {
-      cout << "Node chord has attribute " << pAttr->name() << " ";
-      cout << "with value " << pAttr->value() << "\n";
+  // TODO: set tempo here!
+
+  for(xml_node<>* pChordNode = pRootNode->first_node("chord"); pChordNode; pChordNode = pChordNode->next_sibling()) {
+    for(xml_node<>* pNoteNode = pChordNode->first_node(); pNoteNode; pNoteNode = pNoteNode->next_sibling()) {
+      // print(std::cout, *pNode, 0);
+    
+      printf("%s: %s\n", pNoteNode->name(), pNoteNode->value());
     }
   }
 
